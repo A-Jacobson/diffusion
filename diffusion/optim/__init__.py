0809873e357a7b65fb4452074963a6b9a87d8ed0
austin <jacobsonaustinj@gmail.com>
2023-10-24 19:21:39 -0700

    add mormal lion

--- diffusion/optim/__init__.py
+++ diffusion/optim/__init__.py
@@ -1,3 +1,4 @@
 from diffusion.optim.lion8b import DecoupledLionW_8bit
+from diffusion.optim.lion import DecoupledLionW
 
-__all__ = ['DecoupledLionW_8bit']
+__all__ = ['DecoupledLionW_8bit', 'DecoupledLionW']

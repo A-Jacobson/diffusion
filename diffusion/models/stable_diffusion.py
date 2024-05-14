831170fccfca807d929a1a5f978a871183ccf2d5
austin <jacobsonaustinj@gmail.com>
2024-05-14 16:44:31 -0700

    wrap text encoder

--- diffusion/models/stable_diffusion.py
+++ diffusion/models/stable_diffusion.py
@@ -134,7 +134,7 @@ class StableDiffusion(ComposerModel):
             self.vae = self.vae.half()
         if fsdp:
             # only wrap models we are training
-            self.text_encoder._fsdp_wrap = False
+            self.text_encoder._fsdp_wrap = True
             self.vae._fsdp_wrap = False
             self.unet._fsdp_wrap = True
 
